4d24899d6dcaf6f6a45262052fae0468e228b489
Segasonics <114384404+Segasonics@users.noreply.github.com>
2024-07-09 20:49:39 +0530

    connection to mong and listening to port success

---
+++ src/index.js
@@ -0,0 +1,39 @@
+//require('dotenv').config({path:'./env'})
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv';
+import app from "./app.js";
+
+dotenv.config({path:'./env'})
+
+connectDB()
+.then(()=>{
+   app.listen(process.env.PORT || 8000,()=>{
+    console.log("Mongodb connection was successful to port",process.env.PORT)
+   })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed",err)
+})
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+
+// const app = express();
+
+// (async()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error",(error)=>{
+//           console.log("Error",error);
+//           throw error;
+//        })
+//        app.listen(process.env.PORT,()=>{
+//         console.log(`App is listening on port ${process.env.PORT}`)
+//        })
+//     } catch (error) {
+//         console.log("EROR :",error);
+//         throw err
+//     }
+// })()

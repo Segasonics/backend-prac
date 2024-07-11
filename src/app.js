41bc0d6aaabc15b39642a4d864d3735e04ffa075
Segasonics <114384404+Segasonics@users.noreply.github.com>
2024-07-11 07:05:51 +0530

    utils added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+//parse it and convert it into a JavaScript object.
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+//set and access users browser cookies
+app.use(cookieParser());
+
+
 
 export default app
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+//learn construct0r
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+export { ApiResponse }
